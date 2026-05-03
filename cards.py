620b3b78eb39553829bdd88a35dd2842fba2daf3
jxgcisid-netizen <jxgcisid@gmail.com>
2026-05-03 14:18:20 +0200

    Update cards.py

--- cards.py
+++ cards.py
@@ -184,7 +184,7 @@ async def create_goodbye_card(member, member_count):
 
 async def create_rank_card(member, level, xp, needed_xp, rank):
     # 1. 加载底图
-    bg_path = os.path.join(os.path.dirname(__file__), "Gemini_Generated_Image_t7n65kt7n65kt7n6.png")
+    bg_path = os.path.join(os.path.dirname(__file__), "ezeznoob.png")
     try:
         img = Image.open(bg_path).convert("RGBA")
         img = img.resize((900, 220))
